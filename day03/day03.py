53a213fe5edfc4f18bb6fa8e74f80e496b74b0a9
Samuel Holgersson <samuel.holgersson@gmail.com>
2020-12-03 11:25:14 +0100

    day 3 part 2 done

--- day03/day03.py
+++ day03/day03.py
@@ -1,26 +1,34 @@
 import sys
 
-def get_input1():
+def get_input(x_step, y_step):
     with open(sys.argv[2], 'r') as f:
         for i, line in enumerate(f):
-            yield parse_line1(line, i)
+            if i % y_step == 0:
+                yield parse_line(line, i // y_step, x_step)
     return
 
-def parse_line1(line, line_no):
-    char = line[(line_no * 3) % (len(line) - 1)]
+def parse_line(line, line_no, step_length):
+    char = line[(line_no * step_length) % (len(line) - 1)]
     if char == '#':
         return 1
     if char == '.':
         return 0
 
-def part1():
+def count_crashes(x_step, y_step):
     counter = 0
-    for num in get_input1():
+    for num in get_input(x_step, y_step):
         counter += num
     return counter
 
+def part1():
+    return count_crashes(3, 1)
+
 def part2():
-    pass
+    perms = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+    multiple = 1
+    for x_step, y_step in perms:
+        multiple *= count_crashes(x_step, y_step)
+    return multiple
 
 if __name__ == '__main__':
     if sys.argv[1] == '1':
