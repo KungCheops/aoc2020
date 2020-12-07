6303a8df8e715a765df89c24704433e4cff54781
Samuel Holgersson <samuel.holgersson@gmail.com>
2020-12-07 18:55:57 +0100

    day 7 done

--- day07/day07.py
+++ day07/day07.py
@@ -1,70 +1,60 @@
 import sys
 import re
-
-class Node():
-    def __init__(self, name):
-        self.name = name
-        self.children = dict()
-        self.parents = dict()
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __str__(self):
-        return str(self.name)
-
-    def __repr__(self):
-        return "<Node %s>" % self.name
-
-    def add_child(self, child_name, distance=0):
-        self.children[child_name] = distance
-
-    def get_children(self):
-        return self.children
-
-    def add_parent(self, parent_name, distance=0):
-        self.parents[parent] = distance
-
-    def get_parents(self):
-        return self.parents
+from collections import defaultdict
 
 def get_input():
     with open(sys.argv[2], 'r') as f:
-        node_set = set()
+        children = defaultdict(list)
+        parents = defaultdict(list)
+        distances = dict()
         for line in f:
-            yield parse_line(line)
-    return
+            parent_name, child_distances = parse_line(line)
+            for child_name, distance in child_distances.items():
+                children[parent_name].append(child_name)
+                parents[child_name].append(parent_name)
+                distances[(child_name, parent_name)] = distance
+                distances[(parent_name, child_name)] = distance
+    return children, parents, distances
 
 def parse_line(line):
     first_part, second_part = line.strip().split('contain')
-    first_node_name = first_part[:-6]
-    first_node = Node(first_node_name)
+    parent_name = first_part[:-6]
 
-    parser = re.compile('([0-9]+) ([a-z ]*) bags')
+    parser = re.compile('([0-9]+) ([a-z ]*) bag')
     matches = parser.finditer(second_part)
+    children = dict()
 
     for match in matches:
-        number = int(match.group(1))
-        name = match.group(2)
-        first_node.add_child(name, number)
+        distance = int(match.group(1))
+        child_name = match.group(2)
+        children[child_name] = distance
 
-    return first_node
+    return parent_name, children
 
 def part1():
-    nodes = get_input()
-    for node in nodes:
-        print(node)
-        for child, distance in node.get_children().items():
-            print('    ' + child + ' ' + str(distance))
+    _, parents, _ = get_input()
+    all_parents = set()
+    unprocessed = set()
+    unprocessed.add('shiny gold')
+    while len(unprocessed) > 0:
+        current_child = unprocessed.pop()
+        current_parents = parents[current_child]
+        for parent in current_parents:
+            if not parent in all_parents:
+                all_parents.add(parent)
+                unprocessed.add(parent)
+    return len(all_parents)
+
+def bags_holding(bag_child, all_bag_parents):
+    return 1 + sum([bags_holding(parent_bag, all_bag_parents) for parent_bag in all_bag_parents])
 
 def part2():
-    pass
+    all_bag_children, _, distances = get_input()
+    total_bags = bags_inside('shiny gold', all_bag_children, distances)
+    return total_bags - 1
+
+def bags_inside(bag_parent, all_bag_children, bag_numbers):
+    return 1 + sum([bag_numbers[(bag_parent, child_bag)] * bags_inside(child_bag, all_bag_children, bag_numbers) for child_bag in all_bag_children[bag_parent]])
 
 if __name__ == '__main__':
     if sys.argv[1] == '1':
