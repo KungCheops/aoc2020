eee13e898b7cf4e6d572dfac810eda27bf0b2c0e
Samuel Holgersson <samuel.holgersson@gmail.com>
2020-12-12 01:51:18 +0100

    day 11 part 2 done

--- day11/day11.py
+++ day11/day11.py
@@ -3,10 +3,11 @@ import time
 import numpy as np
 
 class ConwayBoard():
-    def __init__(self, board):
+    def __init__(self, board, part):
         self.board = np.array(board, dtype=int)
         self.occupied_neighbors = np.empty_like(self.board)
         self.dimensions = self.board.shape
+        self.part = part
 
     def __str__(self):
         return str(self.board)
@@ -14,7 +15,10 @@ class ConwayBoard():
     def compute_neighbors(self):
         for i, line in enumerate(self.board):
             for j, element in enumerate(line):
-                self.occupied_neighbors[i][j] = self.get_occupied_neighbors(i, j)
+                if self.part == 1:
+                    self.occupied_neighbors[i][j] = self.get_occupied_neighbors(i, j)
+                elif self.part == 2:
+                    self.occupied_neighbors[i][j] = self.get_occupied_neighbors_far(i, j)
 
     def get_occupied_neighbors(self, x, y):
         counter = 0
@@ -25,7 +29,22 @@ class ConwayBoard():
         return counter
 
     def get_occupied_neighbors_far(self, x, y):
-        pass
+        counter = 0
+        dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        for i, j in dirs:
+            while True:
+                if x + i >= 0 and x + i < self.dimensions[0] and y + j >= 0 and y + j < self.dimensions[1]:
+                    if self.board[x + i][y + j] == 2:
+                        counter += 1
+                        break
+                    elif self.board[x + i][y + j] == 1:
+                        break
+                    else:
+                        i += np.sign(i)
+                        j += np.sign(j)
+                else:
+                    break
+        return counter
 
     def update_board(self):
         self.compute_neighbors()
@@ -35,7 +54,7 @@ class ConwayBoard():
                 if element == 1 and self.occupied_neighbors[i][j] == 0:
                     self.board[i][j] = 2
                     changed = True
-                elif element == 2 and self.occupied_neighbors[i][j] >= 4:
+                elif element == 2 and ((self.part == 1 and self.occupied_neighbors[i][j] >= 4) or (self.part == 2 and self.occupied_neighbors[i][j] >= 5)):
                     self.board[i][j] = 1
                     changed = True
         return changed
@@ -58,7 +77,7 @@ def parse_line(line):
     return list(map(char_to_int, line.strip()))
 
 def part1():
-    conway_board = ConwayBoard(list(get_input()))
+    conway_board = ConwayBoard(list(get_input()), 1)
     counter = 0
     while conway_board.update_board():
         print(f'{counter}  ', end='\r')
@@ -66,7 +85,12 @@ def part1():
     return sum([sum([1 for elem in line if elem == 2]) for line in conway_board.board])
 
 def part2():
-    pass
+    conway_board = ConwayBoard(list(get_input()), 2)
+    counter = 0
+    while conway_board.update_board():
+        print(f'{counter}  ', end='\r')
+        counter += 1
+    return sum([sum([1 for elem in line if elem == 2]) for line in conway_board.board])
 
 if __name__ == '__main__':
     if sys.argv[1] == '1':
