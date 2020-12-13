88c83453605edfbcf12b4d6d907bef3cc3b0e3a5
Samuel Holgersson <samuel.holgersson@gmail.com>
2020-12-13 15:12:14 +0100

    day 13 part 2 done

--- day13/day13.py
+++ day13/day13.py
@@ -1,4 +1,6 @@
 import sys
+import itertools
+import numpy as np
 
 def get_input():
     with open(sys.argv[2], 'r') as f:
@@ -24,8 +26,34 @@ def part1():
             earliest_bus_id = bus_id
     return shortest_wait_time * earliest_bus_id
 
+def possible_wait_times(bus_id, offset):
+    i = 0
+    while True:
+        ret = i * bus_id - offset
+        while ret < offset:
+            i += 1
+            ret = i * bus_id - offset
+        yield ret
+        i += 1
+
 def part2():
-    pass
+    _, line2 = get_input()
+    bus_ids = list()
+    offsets = list()
+    for i, bus_id in enumerate(line2.strip().split(',')):
+        if bus_id.isnumeric():
+            bus_ids.append(int(bus_id))
+            offsets.append(i)
+
+    multiple = bus_ids[0]
+    counter = 0
+    for i in range(1, len(bus_ids)):
+        while True:
+            if (counter + offsets[i]) % bus_ids[i] == 0:
+                break
+            counter += multiple
+        multiple *= bus_ids[i]
+    return counter
 
 if __name__ == '__main__':
     if sys.argv[1] == '1':
