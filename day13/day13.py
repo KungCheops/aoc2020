aead5e309a966e7e4669469b6aed359b1cc8618b
Samuel Holgersson <samuel.holgersson@gmail.com>
2020-12-13 12:30:40 +0100

    Day 13 part 1 done.

--- day13/day13.py
+++ day13/day13.py
@@ -10,7 +10,19 @@ def parse_line(line):
     return line
 
 def part1():
-    pass
+    line1, line2 = get_input()
+    earliest_departure = int(line1.strip())
+    bus_ids = list(map(int, (bus_time for bus_time in line2.strip().split(',') if bus_time.isnumeric())))
+
+    shortest_wait_time = float('inf')
+    earliest_bus_id = -1
+    for bus_id in bus_ids:
+        wait_time = bus_id - (earliest_departure % bus_id)
+        # print(bus_id, wait_time)
+        if wait_time < shortest_wait_time:
+            shortest_wait_time = wait_time
+            earliest_bus_id = bus_id
+    return shortest_wait_time * earliest_bus_id
 
 def part2():
     pass
