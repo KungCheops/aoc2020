2b1f65466a7d289697e743ec45cdfb45509defc0
Samuel Holgersson <samuel.holgersson@gmail.com>
2020-12-14 13:01:09 +0100

    day 14 part 1 done

--- day14/day14.py
+++ day14/day14.py
@@ -1,5 +1,23 @@
 import sys
 
+class Computer():
+    def __init__(self):
+        self.memory = dict()
+        self.and_mask = 0xfffffffff
+        self.or_mask = 0x000000000
+
+    def update_masks(self, mask):
+        and_mask_string = mask.replace('X', '1')
+        self.and_mask = int(and_mask_string, 2)
+        or_mask_string = mask.replace('X', '0')
+        self.or_mask = int(or_mask_string, 2)
+
+    def write_to_memory(self, position, data):
+        self.memory[position] = (data & self.and_mask) | self.or_mask
+
+    def get_memory_sum(self):
+        return sum(self.memory.values())
+
 def get_input():
     with open(sys.argv[2], 'r') as f:
         for line in f:
@@ -7,10 +25,16 @@ def get_input():
     return
 
 def parse_line(line):
-    return line
+    return line.strip().split(' = ')
 
 def part1():
-    pass
+    computer = Computer()
+    for instruction in get_input():
+        if instruction[0] == 'mask':
+            computer.update_masks(instruction[1])
+        elif instruction[0].startswith('mem'):
+            computer.write_to_memory(int(instruction[0][4:-1]), int(instruction[1]))
+    return computer.get_memory_sum()
 
 def part2():
     pass
