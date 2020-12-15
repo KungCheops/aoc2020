8bef8c1b2b3a7cbbcd08aa28f39b5653e78e5d6d
Samuel Holgersson <samuel.holgersson@gmail.com>
2020-12-15 13:44:57 +0100

    day 15 done

--- day15/day15.py
+++ day15/day15.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict, deque
 
 def get_input():
     with open(sys.argv[2], 'r') as f:
@@ -7,13 +8,35 @@ def get_input():
     return
 
 def parse_line(line):
-    return line
+    return line.strip().split(',')
+
+def find_number(initial_list, number):
+    last_spoken_at = defaultdict(deque)
+    for iter, initial_number in enumerate(initial_list):
+        last_spoken_at[initial_number].append(iter)
+        # print(iter + 1, initial_number)
+    last_spoken_number = initial_list[-1]
+    for iter in range(len(initial_list), number):
+        if len(last_spoken_at[last_spoken_number]) == 1:
+            next_number = 0
+        else:
+            next_number = last_spoken_at[last_spoken_number][1] - last_spoken_at[last_spoken_number].popleft()
+        last_spoken_at[next_number].append(iter)
+        last_spoken_number = next_number
+        # print(iter + 1, next_number)
+    return last_spoken_number
+
+def nth_number_spoken(n):
+    answers = list(find_number(list(map(int, numbers)), n) for numbers in get_input())
+    if len(answers) == 1:
+        return answers[0]
+    return answers
 
 def part1():
-    pass
+    return nth_number_spoken(2020)
 
 def part2():
-    pass
+    return nth_number_spoken(30000000)
 
 if __name__ == '__main__':
     if sys.argv[1] == '1':
