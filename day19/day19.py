021e99d7a0ad14e4f58627ef47de1d4737f44cdb
Samuel Holgersson <samuel.holgersson@gmail.com>
2020-12-20 13:12:51 +0100

    part 2 almost works

--- day19/day19.py
+++ day19/day19.py
@@ -11,35 +11,40 @@ def parse_line(line, ):
     return line.replace('"', '').strip()
 
 def match(string, rules, rule='0'):
-    # print(f'matching string={string}, rule={rule}')
+    print(f'matching string={string}, rule={rule}')
     if len(string) == 0:
-        return True
+        return (True, string, rule)
     if isinstance(rule, str):
         if rule.isalpha():
-            return (string[0] == rule, string[1:])
+            return (string[0] == rule, string[1:], rule)
         elif rule.isnumeric():
-            return match(string, rules, rules[rule])
+            match_, rest, rule2 = match(string, rules, rules[rule])
+            return match_, rest, (rule, rule2)
     elif isinstance(rule, tuple):
         left_rule, right_rule = rule
-        left_match, rest = match(string, rules, left_rule)
+        left_match, rest, rule = match(string, rules, left_rule)
         if left_match:
-            return left_match, rest
-        right_match, rest = match(string, rules, right_rule)
+            return left_match, rest, rule
+        right_match, rest, rule = match(string, rules, right_rule)
         if right_match:
-            return right_match, rest
-        return False, string
+            return right_match, rest, rule
+        return False, string, rule
     elif isinstance(rule, list):
+        rules_used = []
         for rule_elem in rule:
-            match_, string = match(string, rules, rule_elem)
+            #if len(string) == 0:
+            #    return (False, string)
+            match_, string, rule2 = match(string, rules, rule_elem)
+            rules_used.append(rule2)
             if not match_:
-                return (False, string)
-        return (True, string)
+                return (False, string, rules_used)
+        return (True, string, rules_used)
 
-def part1():
+def count_matches(input):
     state = 0
     rules = dict()
     count = 0
-    for line in get_input():
+    for line in input:
         if state == 0:
             if line == '':
                 state = 1
@@ -55,13 +60,23 @@ def part1():
                 else:
                     rules[rule_number] = rule.split(' ')
         if state == 1:
-            match_, rest = match(line, rules)
+            match_, rest, rule = match(line, rules)
             if match_ and len(rest) == 0:
+                print(line, rule)
                 count += 1
     return count
 
+def part1():
+    return count_matches(get_input())
+
 def part2():
-    pass
+    input = list(get_input())
+    for i, line in enumerate(input):
+        if line.startswith('8:'):
+            input[i] = '8: 42 | 42 8'
+        elif line.startswith('11:'):
+            input[i] = '11: 42 31 | 42 11 31'
+    return count_matches(input)
 
 if __name__ == '__main__':
     if sys.argv[1] == '1':
